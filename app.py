70a3c2e063cac55d96ec800e734c3101451a1dab
RupeshKeesaram <keesaramrupesh@gmail.com>
2021-09-27 12:44:07 +0530

    app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def fetch():
     data= web_scrapper(name,count)
     return render_template("results.html",data=data,mobile_name=name)
 
-app.run(debug = True)
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='127.0.0.1', port=port)
